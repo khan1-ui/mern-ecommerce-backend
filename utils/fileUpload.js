6bf81d61a4a1126e79fd46c64a0aa90ba429c874
Md.Shajin Khan <mdsazinur90@yahoo.com>
2026-02-11 12:56:41 +0600

    upload file

--- utils/fileUpload.js
+++ utils/fileUpload.js
@@ -1,13 +1,28 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+const __dirname = path.resolve();
+
+// ================= CREATE FOLDERS =================
+const imagePath = path.join(__dirname, "uploads/images");
+const filePath = path.join(__dirname, "uploads/files");
+
+if (!fs.existsSync(imagePath)) {
+  fs.mkdirSync(imagePath, { recursive: true });
+}
+
+if (!fs.existsSync(filePath)) {
+  fs.mkdirSync(filePath, { recursive: true });
+}
 
 // ================= STORAGE =================
 const storage = multer.diskStorage({
   destination(req, file, cb) {
     if (file.fieldname === "images") {
-      cb(null, "uploads/images");
+      cb(null, imagePath);
     } else if (file.fieldname === "file") {
-      cb(null, "uploads/files");
+      cb(null, filePath);
     } else {
       cb(new Error("Invalid field"), false);
     }
